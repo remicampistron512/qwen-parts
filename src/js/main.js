e4b1acf66fd94e1298363e32fc292d2d9d7f02ed
Rémi Campistron <remi.campistron@gmail.com>
2026-01-20 15:56:18 +0100

    Creation de l'html pour la page d'accueil

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
