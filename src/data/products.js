80998c032b7e5823306e12cfac05ba7b96f0e7f1
Rémi Campistron <remi.campistron@gmail.com>
2026-01-21 09:47:01 +0100

    separation de products dans un fichier séparé

---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        category: "motherboard",
+        title: "ASUS B550",
+        description: "AM4 ATX motherboard with PCIe 4.0",
+        price: 149.99,
+        img: "img/motherboard.jpg",
+    },
+    {
+        id: 2,
+        category: "motherboard",
+        title: "MSI B760",
+        description: "Intel LGA1700 motherboard for 12th/13th gen",
+        price: 169.99,
+        img: "img/motherboard2.jpg",
+    },
+    {
+        id: 3,
+        category: "cpu",
+        title: "Ryzen 7 5800X",
+        description: "8 cores / 16 threads",
+        price: 229.99,
+        img: "img/ryzen-7.jpg",
+    },
+    {
+        id: 4,
+        category: "psu",
+        title: "Corsair RM750",
+        description: "750W Gold modular PSU",
+        price: 119.99,
+        img: "img/power-supply.jpg",
+    },
+];
--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ import * as bootstrap from "bootstrap";
 // Import Bootstrap Icons CSS
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
+import { PRODUCTS } from "../data/products.js"
+
 // =============================
 // DOM References
 // =============================
@@ -22,40 +24,6 @@ const mainContainer = document.querySelector("#main-container"); // Used to prev
 // Data (Products)
 // =============================
 
-const PRODUCTS = [
-    {
-        id: 1,
-        category: "motherboard",
-        title: "ASUS B550",
-        description: "AM4 ATX motherboard with PCIe 4.0",
-        price: 149.99,
-        img: "img/motherboard.jpg",
-    },
-    {
-        id: 2,
-        category: "motherboard",
-        title: "MSI B760",
-        description: "Intel LGA1700 motherboard for 12th/13th gen",
-        price: 169.99,
-        img: "img/motherboard2.jpg",
-    },
-    {
-        id: 3,
-        category: "cpu",
-        title: "Ryzen 7 5800X",
-        description: "8 cores / 16 threads",
-        price: 229.99,
-        img: "img/ryzen-7.jpg",
-    },
-    {
-        id: 4,
-        category: "psu",
-        title: "Corsair RM750",
-        description: "750W Gold modular PSU",
-        price: 119.99,
-        img: "img/power-supply.jpg",
-    },
-];
 
 // =============================
 // Cart LocalStorage Service
